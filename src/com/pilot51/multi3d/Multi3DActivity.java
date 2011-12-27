c0e91adae3e40d16d32d0749b6d1f111ea6da47e
Pilot_51 <pilota51@gmail.com>
2011-12-27 18:59:06 -0500

    Initial commit.
    Fresh project.

---
+++ src/com/pilot51/multi3d/Multi3DActivity.java
@@ -0,0 +1,13 @@
+package com.pilot51.multi3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Multi3DActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
